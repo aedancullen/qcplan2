69ba5bcab589cc99ddb031aacb96b31f004ac803
Aedan Cullen <aedan@aedancullen.com>
2022-04-24 13:51:03 -0500

    Finish gamepadpipe

---
+++ gamepadpipe.py
@@ -0,0 +1,49 @@
+import threading
+
+pipe = open("gamepadpipe", 'r')
+data = {
+    "left_stick_x": 0,
+    "left_stick_y": 0,
+    "right_stick_x": 0,
+    "right_stick_y": 0,
+    "x": 0,
+    "y": 0,
+    "a": 0,
+    "b": 0,
+}
+
+def scale_js(value):
+    return value / 32768
+
+def get_data():
+    return data
+
+def pipe_thread():
+    global data
+
+    line = pipe.readline()
+    while line != "":
+        if "value" in line:
+            value = int(line.split("value")[-1])
+            if "ABS_X" in line:
+                data["left_stick_x"] = scale_js(value) if value != 128 else 0
+            elif "ABS_Y" in line:
+                data["left_stick_y"] = scale_js(value) if value != -129 else 0
+            elif "ABS_RX" in line:
+                data["right_stick_x"] = scale_js(value) if value != 128 else 0
+            elif "ABS_RY" in line:
+                data["right_stick_y"] = scale_js(value) if value != -129 else 0
+            elif "BTN_NORTH" in line:
+                data["x"] = value
+            elif "BTN_WEST" in line:
+                data["y"] = value
+            elif "BTN_SOUTH" in line:
+                data["a"] = value
+            elif "BTN_EAST" in line:
+                data["b"] = value
+        line = pipe.readline()
+    data = None
+
+pipe_daemon = threading.Thread(target=pipe_thread)
+pipe_daemon.setDaemon(True)
+pipe_daemon.start()
