e1b539b8ce35540a600e48fee5e2e2f362ffd33b
Aedan Cullen <aedan@aedancullen.com>
2022-05-08 23:04:26 -0700

    Start adding transform stuff

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import maestrocar
 
 import rospy
 import tf2_ros
+from transformations inport euler_from_quaternion
 
 from ompl import util as ou
 from ompl import base as ob
@@ -42,8 +43,40 @@ class QCPlan2:
     def __init__(self):
         pass
 
+last_transform = None
+last_time = None
+
 try:
     while True:
+        transform = input_map.get_transform()
+        time = rospy.Time()
+
+        if transform is not None and last_transform is not None and time is not None and last_time is not None:
+            time_diff = time - last_time
+            sref = self.state()
+            sref[0].setX(transform.translation.x)
+            sref[0].setY(transform.translation.y)
+            x, y, z = euler_from_quaternion([
+                transform.rotation.x,
+                transform.rotation.y,
+                transform.rotation.z,
+                transform.rotation.w,
+            ])
+            sref[0].setYaw(z)
+            sref[1][0] = 0 / time_diff
+            sref[1][1] = 0 / time_diff
+            rotation_diff = last_transform.rotation.inverseTimes(transform.rotation)
+            x, y, z = euler_from_quaternion([
+                rotation_diff.x,
+                rotation_diff.y,
+                rotation_diff.z,
+                rotation_diff.w,
+            ])
+            sref[1][2] = z / time_diff
+
+        last_transform = transform
+        last_time = time
+
         if gamepadpipe.get_updated():
             gpdata = gamepadpipe.get_data()
             if gpdata is None:
@@ -52,6 +85,7 @@ try:
             steering = gpdata["right_stick_x"]
             maestrocar.set_control(accelerator, steering)
         time.sleep(0.001)
+
 except KeyboardInterrupt:
     pass
 
