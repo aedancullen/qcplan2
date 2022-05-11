9383e935c98f7f2b8fbdc74ec3dd9e47a81c59fc
Aedan Cullen <aedan@aedancullen.com>
2022-05-11 00:31:31 -0500

    Add mainloop structure

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import maestrocar
 
 import rospy
 import tf2_ros
-from transformations inport euler_from_quaternion
+from tf.transformations import euler_from_quaternion
 
 from ompl import util as ou
 from ompl import base as ob
@@ -29,9 +29,8 @@ GOAL_THRESHOLD = 0.5
 
 class InputMap:
     def __init__(self):
-        rospy.init_node("qcplan2")
         self.transform_buffer = tf2_ros.Buffer()
-        self.transform_listener = tf2_ros.TransformListener(transform_buffer)
+        self.transform_listener = tf2_ros.TransformListener(self.transform_buffer)
 
     def get_transform(self):
         try:
@@ -40,19 +39,17 @@ class InputMap:
             return None
 
 class QCPlan2:
-    def __init__(self):
-        pass
+    def __init__(self, input_map):
+        self.input_map = input_map
+        self.last_transform = None
+        self.last_timestamp = None
 
-last_transform = None
-last_time = None
+    def loop(self):
+        transform = self.input_map.get_transform()
+        timestamp = rospy.Time()
 
-try:
-    while True:
-        transform = input_map.get_transform()
-        time = rospy.Time()
-
-        if transform is not None and last_transform is not None and time is not None and last_time is not None:
-            time_diff = time - last_time
+        if transform is not None and self.last_transform is not None and timestamp is not None and self.last_timestamp is not None:
+            timestamp_diff = timestamp - self.last_timestamp
             sref = self.state()
             sref[0].setX(transform.translation.x)
             sref[0].setY(transform.translation.y)
@@ -65,31 +62,38 @@ try:
             sref[0].setYaw(z)
             sref[1][0] = 0 / time_diff
             sref[1][1] = 0 / time_diff
-            rotation_diff = last_transform.rotation.inverseTimes(transform.rotation)
+            rotation_diff = self.last_transform.rotation.inverseTimes(transform.rotation)
             x, y, z = euler_from_quaternion([
                 rotation_diff.x,
                 rotation_diff.y,
                 rotation_diff.z,
                 rotation_diff.w,
             ])
-            sref[1][2] = z / time_diff
+            sref[1][2] = z / timestamp_diff
 
-        last_transform = transform
-        last_time = time
+        self.last_transform = transform
+        self.last_timestamp = timestamp
 
         if gamepadpipe.get_updated():
             gpdata = gamepadpipe.get_data()
             if gpdata is None:
-                break
-            accelerator = min(-gpdata["left_stick_y"], 0.25)
+                return  -1
+            accelerator = max(min(-gpdata["left_stick_y"], 0.25), -0.25)
             steering = gpdata["right_stick_x"]
             maestrocar.set_control(accelerator, steering)
-        time.sleep(0.001)
 
-except KeyboardInterrupt:
-    pass
+        return 0
 
-maestrocar.set_control(0, 0)
-
-gamepadproc.send_signal(signal.SIGTERM)
-gamepadproc.wait()
+if __name__ == "__main__":
+    rospy.init_node("qcplan2")
+    input_map = InputMap()
+    qc = QCPlan2(input_map)
+    try:
+        while not rospy.is_shutdown():
+            if qc.loop() != 0:
+                break
+    except Exception as e:
+        print(e)
+    maestrocar.set_control(0, 0)
+    gamepadproc.send_signal(signal.SIGTERM)
+    gamepadproc.wait()
